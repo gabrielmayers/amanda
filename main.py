79fa0c21475b8e752e8cd515693b854d0ede4576
Gabriel Mayer <43456673+gabrielmayers@users.noreply.github.com>
2020-06-28 19:23:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import tweepy
 
 # Authenticate to Twitter
 
-auth = tweepy.OAuthHandler("6vb95s6RCO5VS3WbI9VtD8KA0", "BsbMbrjnThkQhd2W14tZuTTFqUyO5F10bTJOS9AFb8aFu4F5A2")
+auth = tweepy.OAuthHandler("XXXXXXXXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXXXXXXX")
 
-auth.set_access_token("1262531460894011395-Xd9xt4PacAbowV7JOp1qrzhC6EKJbM", "5nb9Z1qMuS4qrlYzm1QwE8xOflhs9sOAQtK4O42QKTqSe")
+auth.set_access_token("XXXXXXXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXXXXXX")
 
 api = tweepy.API(auth)
 
