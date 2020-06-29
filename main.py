d3894b3dd4b0ab043df6e5bd7cc4a121edc46d40
gabrielmayers <gabriel.mayers@hotmail.com>
2020-06-29 18:15:01 -0300

    retweet optimized, baby

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import tweepy
 
-# Authenticate to Twitter
 
-auth = tweepy.OAuthHandler("09nFtECOjaCCvL3JQjJcxvVLt", "MplwsC879nCoFffxwXP1oWRRfD4l990ZK0o2tuE4mty9rXxS5N")
+# Auth:
 
-auth.set_access_token("1277383591014563840-ZJNhopLOEajbdwq0k8wc6fmqSyST1Y", "cGsbRZzZOB0lM6AY24774DaxBlrTgVA6KSakp1qmr8EWn")
+def auth_twitter(api_key, api_secret_key, access_tok, access_tok_secret):
+    auth = tweepy.OAuthHandler(api_key, api_secret_key)
+
+    auth.set_access_token(access_tok, access_tok_secret)
+
+    return auth
+
+
+auth = auth_twitter('09nFtECOjaCCvL3JQjJcxvVLt', 'MplwsC879nCoFffxwXP1oWRRfD4l990ZK0o2tuE4mty9rXxS5N',
+                    '1277383591014563840-ZJNhopLOEajbdwq0k8wc6fmqSyST1Y',
+                    'cGsbRZzZOB0lM6AY24774DaxBlrTgVA6KSakp1qmr8EWn')
 
 api = tweepy.API(auth, wait_on_rate_limit_notify=True, wait_on_rate_limit=True)
 
-for i in api.search(q='#machinelearning', count=2):
-    api.retweet(i.id)
+
+# Make Retweets:
+
+def make_ret(api_ref, keyword, n):
+    for i in api_ref.search(q=keyword, count=n):
+        api_ref.retweet(i.id)
+
+
+make_ret(api, '#deeplearning', 1)
