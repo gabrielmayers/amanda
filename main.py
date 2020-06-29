fa09d4c74f1af94e2cbe8d891b1a9cf759c757e0
gabrielmayers <gabriel.mayers@hotmail.com>
2020-06-29 18:01:51 -0300

    retweet on, baby

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ import tweepy
 
 auth = tweepy.OAuthHandler("09nFtECOjaCCvL3JQjJcxvVLt", "MplwsC879nCoFffxwXP1oWRRfD4l990ZK0o2tuE4mty9rXxS5N")
 
-auth.set_access_token("1277383591014563840-ZJNhopLOEajbdwq0k8wc6fmqSyST1Y", "cGsbRZzZOB0lM6AY24774DaxBlrTgVA6KSakp1qmr8EWn")
+auth.set_access_token("1277383591014563840-ZJNhopLOEajbdwq0k8wc6fmqSyST1Y",
+                      "cGsbRZzZOB0lM6AY24774DaxBlrTgVA6KSakp1qmr8EWn")
 
 api = tweepy.API(auth, wait_on_rate_limit_notify=True, wait_on_rate_limit=True)
 
-api.update_status("Testing Tweet!")
+# search_res = api.search(q='#machinelearning', count=5)
+
+# print(type(search_res))
+
+for i in api.search(q='#machinelearning', count=2):
+    api.retweet(i.id)
+
+#api.retweet(1277707402801020930)
