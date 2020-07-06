96844549b65eecfdc48bbb363461c8d696f31fe5
gabrielmayers <gabriel.mayers@hotmail.com>
2020-07-06 16:33:26 -0300

    preventing spam

--- main.py
+++ main.py
@@ -23,15 +23,36 @@ api = tweepy.API(auth, wait_on_rate_limit_notify=True, wait_on_rate_limit=True)
 
 def make_rt(keywords, sleep):
     search_results = api.search(q=[keywords], count=100)
+    call = call_api(1)
 
     for i in search_results:
 
         try:
             api.retweet(id=i.id, count=1)
-            print('Retweeted!')
+
+            call = call + 1
+
             time.sleep(sleep)
         except:
             pass
+
+    verify_call(call)
+
+
+# Function to count each API call and prevent spam:
+
+def call_api(call):
+    call = call + 1
+    return call
+
+
+# Verify number of call and prevent spam:
+
+def verify_call(call):
+    if call > 50:
+        time.sleep(900)
+    else:
+        pass
 
 
 make_rt(keywords=['#machinelearning', '#deeplearning', '#ai', '#artificialintelligence'],
