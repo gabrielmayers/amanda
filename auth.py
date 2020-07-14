c86d898bc2aeb98fb3b80d04e348fb86f6487f1a
Gabriel Mayer <43456673+gabrielmayers@users.noreply.github.com>
2020-07-14 19:55:47 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -14,9 +14,9 @@ def auth_data(api_key, api_secret_key, access_tok, access_tok_secret):
 
 
 def auth_twitter_api():
-    auth = auth_data('09nFtECOjaCCvL3JQjJcxvVLt', 'MplwsC879nCoFffxwXP1oWRRfD4l990ZK0o2tuE4mty9rXxS5N',
-                     '1277383591014563840-AofHavFKYrqbiDpvsQKsXAmJq3F64L',
-                     'zkwBIPOywrHLAsEVjEJsAUtVXT3pSOn8GCAP72vO6Xa22')
+    auth = auth_data('XXXXXXXXXXXXXXXXXXXXXXXXX', 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+                     'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+                     'XXXXXXXXXXXXXXXXXXXXXXXXX')
 
     api = tweepy.API(auth, wait_on_rate_limit_notify=True, wait_on_rate_limit=True)
 
