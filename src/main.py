fb26ea09f57aaa98b3e428f050bb20f483aed224
gabrielmayers <gabriel.mayers@hotmail.com>
2020-09-01 14:07:29 -0300

    .

--- src/main.py
+++ src/main.py
@@ -8,10 +8,3 @@ from retweet import *
 # search_tweets(keywords=['#machinelearning', '#deeplearning', '#ai'])
 
 # Reply mentions and send thank u message:
-
-since_id = 1
-
-while True:
-    since_id = check_mentions(["@AmieAndYou"], since_id)
-    print("Waiting...")
-    time.sleep(60)
