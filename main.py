170f4f1e4114bfdf0cddd81ba39a8dcbb347bf61
gabrielmayers <gabriel.mayers@hotmail.com>
2020-09-01 12:40:29 -0300

    project updated

--- main.py
+++
@@ -1,9 +0,0 @@
-""" The main file """
-
-# Make Retweets:
-
-# search_tweets(keywords=['#machinelearning', '#deeplearning', '#ai'])
-
-# Reply mentions and send thank u message:
-
-# search_mention(username=['@AmieAndYou'])
---
+++ src/main.py
@@ -0,0 +1,12 @@
+""" The main file """
+
+from interactions import *
+from retweet import *
+
+# Make Retweets:
+
+search_tweets(keywords=['#machinelearning', '#deeplearning', '#ai'])
+
+# Reply mentions and send thank u message:
+
+# search_mention(username=['@AmieAndYou'])
